39ffd2f1e0834009e0dc016671c6400251095bd8
enmanuelar <ealmonterozon@gmail.com>
2016-08-15 16:30:34 -0400

    hw lesson2

---
+++ app/controllers/contact.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	emailAddress: '',
+	message: '',
+	isValidEmail: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+	isValidMessage: Ember.computed.gte('message.length', 5),
+	isValid: Ember.computed.and('isValidEmail', 'isValidMessage'),
+	isDisabled: Ember.computed.not('isValid'),
+	actions: {
+		sendMessage: function(){
+			alert(`sent message ${this.get('message')} from ${this.get('emailAddress')}`);
+			this.set('responseMessage', `Thanks for submitting your message`);
+			this.set('message', '');
+		}
+	}
+});
---
+++ app/controllers/index.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  emailAddress: '',
+
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+
+  isDisabled: Ember.computed.not('isValid'),
+
+  actions: {
+  	saveInvitation: function(){
+  		alert(`saving email address... ${this.get('emailAddress')}`);
+  		this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+  		this.set('emailAddress', '');
+  	}	
+  }
+});
