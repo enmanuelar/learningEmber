a17b36a091fbbfb25c9f3ccde8d35cec7e76cfc4
enmanuelar <ealmonterozon@gmail.com>
2016-08-17 16:11:20 -0400

    Lesson 4 HW

--- app/routes/admin/contacts.js
+++ app/routes/admin/contacts.js
@@ -3,5 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model() {
     	return this.store.findAll('contact');
+  },
+  actions: {
+  	deleteMessage(message){
+  		let confirmation = confirm("You're going to delete this message, are you sure?");
+  		if (confirmation){
+  			message.destroyRecord();
+  		}
+  	}
   }
 });
